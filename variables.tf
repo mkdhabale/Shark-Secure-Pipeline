63648908f84b48253a2cfb60bfc6430539574ff3
Mukul Dhabale <mukul.dhabale@gmail.com>
2024-05-11 18:52:56 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  type        = string
+  description = "access_key"
+  sensitive = true
+}
+
+variable "secret_key" {
+  type        = string
+  description = "secret_key"
+  sensitive = true
+}
